2ffd1d3c0f8e97c54936c144d61d3e95a414f5f8
Samuel Rodriguez <sam.rod.719@gmail.com>
2022-11-30 10:55:24 -0800

    modularization

---
+++ crud/users_cr/flask_app/controllers/users.py
@@ -0,0 +1,67 @@
+from flask_app import app
+from flask import render_template, redirect, request
+from flask_app.models.user import User
+
+@app.route('/')
+def index():
+    return redirect('/users')
+
+@app.route('/users')
+def display_users():
+    users = User.get_all()
+    print(users)
+    return render_template("index.html", all_users=users)
+
+@app.route('/users/new')
+def add_user():
+    return render_template("add.html")
+
+@app.route('/users/create', methods=['POST'])
+def create_user():
+    data = {
+        "fname": request.form["fname"],
+        "lname": request.form["lname"],
+        "email": request.form["email"]
+    }
+    User.save(data)
+    return redirect('/users')
+
+@app.route('/users/show/<int:id>')
+def display_user(id):
+    data = {
+        "id": id
+    }
+    user = User.display_user(data)
+    print(user)
+    return render_template("show.html", user=user)
+
+@app.route('/users/delete/<int:id>')
+def delete_user(id):
+    data = {
+        "id": id
+    }
+    User.delete_user(data)
+    return redirect('/users')
+
+@app.route('/users/edit/<int:id>')
+def edit_user(id):
+    data = {
+        "id": id
+    }
+    user = User.display_user(data)
+    print(user)
+    return render_template('edit.html', user=user)
+
+@app.route('/users/edit/<int:id>/push', methods=['POST'])
+def push_edit(id):
+    data = {
+        "fname": request.form["fname"],
+        "lname": request.form["lname"],
+        "email": request.form["email"],
+        "id": id
+    }
+    print(data)
+    User.update_user(data)
+    print(id)
+    return redirect(f'/users/show/{data["id"]}')
+    # return redirect('/users')
--- crud/users_cr/flask_app/models/user.py
+++ crud/users_cr/flask_app/models/user.py
@@ -1,5 +1,5 @@
 # import the function that will return an instance of a connection
-from mysqlconnection import connectToMySQL
+from flask_app.config.mysqlconnection import connectToMySQL
 class User:
     def __init__( self , data ):
         self.id = data['id']
