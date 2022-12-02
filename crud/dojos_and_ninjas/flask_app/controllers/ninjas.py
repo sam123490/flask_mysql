851530e41db433a1adc7dd856260fbe81bb5f579
Samuel Rodriguez <sam.rod.719@gmail.com>
2022-12-02 15:44:08 -0800

    removed repeated ninja id

--- crud/dojos_and_ninjas/flask_app/controllers/ninjas.py
+++ crud/dojos_and_ninjas/flask_app/controllers/ninjas.py
@@ -26,7 +26,7 @@ def edit_ninja(dojo_id, ninja_id):
         "id": ninja_id
     }
     ninja = Ninja.get_ninja(data)
-    return render_template('edit-ninja.html', dojo_id=dojo_id, ninja_id=ninja_id, ninja=ninja[0])
+    return render_template('edit-ninja.html', dojo_id=dojo_id, ninja=ninja[0])
 
 @app.route('/ninjas/edit/<int:dojo_id>/<int:ninja_id>/push', methods=['POST'])
 def push_edit(dojo_id, ninja_id):
